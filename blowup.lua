3e4db6316cd77aed930c6e5ecc77d7b59ef79dfa
luasopia <salesiopark@gmail.com>
2021-09-24 17:06:37 +0900

    fix grammar

--- blowup.lua
+++ blowup.lua
@@ -28,7 +28,7 @@ local seqhit = {time=200, loops=1}
 local function updflash(self)
     local ys = self:getyscale() - 0.3 -- 0.3
     if ys<0.1 then ys = 0.1 end
-    self:yscale(ys)
+    self:setyscale(ys)
 end
 
 local function blowup()
@@ -38,19 +38,19 @@ local function blowup()
     -- explosion smoke effect 
     for k=1,8 do
         local s1, dx1, dy1 = rand(10,20)/10, rand(-60,60)/10, rand(-60,60)/10
-        Sprite(sht2,seqflame):addto(grp):scale(s1):dxdy(dx1,dy1):play()
+        Sprite(sht2,seqflame):addto(grp):set{scale=s1,dx=dx1, dy=dy1}:play()
         local dx2, dy2 = rand(-15,15)/10, rand(-15,15)/10
-        Sprite(sht3,seqflame):addto(grp):dxdy(dx2,dy2):play()
+        Sprite(sht3,seqflame):addto(grp):setdxdy(dx2,dy2):play()
     end
    
     -- flash effect
     local flash = Image(u'flash.png'):addto(grp)
-    flash:rot(rand(-30,30)):dxscale(0.4):removeafter(100)
+    flash:setrot(rand(-30,30)):setdxscale(0.4):removeafter(100)
     flash.update = updflash
 
     -- background light effect
     local hit = Sprite(shthit,seqhit):addto(grp)
-    hit:rot(rand(360)):scale(2.2)
+    hit:set{rot=rand(360), scale=2.2}
     hit:play():removeafter(250)
 
     explsnd:play()
